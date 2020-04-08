06890eba4192d8b12fd5c44ab18d4de27f3d5f80
lixianqiu <lixianqiu@shengtang.ai>
2020-04-08 11:41:54 +0800

    create

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+
+int main(void){
+    printf("printf('Hello World!')\n ");
+    std::cout << "std::cout << 'Hello World!' << std::endl"<<std::endl;
+    return 0;
+}
